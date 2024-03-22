eb5300a8393facf433353af422d23838df887877
DuyKhanh202 <lenguyenduykhanh02@gmail.com>
2024-03-22 10:28:49 +0700

    Bài 4-5-6

--- Lab4-5-6/footer.php
+++ Lab4-5-6/footer.php
@@ -4,7 +4,7 @@
 </div>
 <div id="wrapper" class="container">
     <footer>
-        Comeback <a href="/demo/Lab03">trang chủ</a><br />
+        Comeback <a href="/demo/Lab03">Trang Chủ</a><br />
         &#169; 2022 - Trendemy
     </footer>
 </div>
