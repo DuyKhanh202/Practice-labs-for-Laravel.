76f61307ee2659f3eab706a1904a99df481db53e
DuyKhanh202 <lenguyenduykhanh02@gmail.com>
2024-03-22 10:24:03 +0700

    Exercises 1->6 for Laravel.

---
+++ Lab4-5-6/footer.php
@@ -0,0 +1,13 @@
+</div>
+
+</div>
+</div>
+<div id="wrapper" class="container">
+    <footer>
+        Comeback <a href="/demo/Lab03">trang chủ</a><br />
+        &#169; 2022 - Trendemy
+    </footer>
+</div>
+</body>
+
+</html>
